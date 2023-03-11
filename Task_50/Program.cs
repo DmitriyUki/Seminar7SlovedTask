3d3ded8240405174c556d1c86d309c94b08afb86
Dmitriy U <ukolov.d.90@mail.ru>
2023-03-11 14:28:19 +0300

    Sloved task stminar 7

---
+++ Task_50/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 50: Напишите программу, которая на вход принимате позиции элемента в двумерном массиве, 
+//и возвращает значение каждого элемента или же указание что такого элемента нет.
+//1 4 7 2 
+//5 9 2 3 
+//8 4 2 4
+
+//17- такого числа в массиве нет
+
+
+int ReadNumber(string messageToUser)
+{
+  Console.WriteLine(messageToUser);
+  int value = Convert.ToInt32(Console.ReadLine());
+  return value;
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftBorder = 0, int rightBorder = 10)
+{
+  int[,] matrix = new int[rows, columns];
+
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder + 1); // обращаемся к нашему элементу по i и j создаем 
+                                                                      //рандомные значения, задав диапазон
+    }
+  }
+  return matrix;
+}
+
+void MatrixSquare(int[,] matrix, int row, int column) // создаем метод который будет определять элемент  матрице
+{
+  if (row >= matrix.GetLength(0) - 1 || column >= matrix.GetLength(1) - 1) // проверка условия 
+  {
+    Console.WriteLine("Такого элемента в матрице нет");
+  }
+  else
+  {
+    Console.WriteLine($"значение элемента равно {matrix[row, column]}");
+  }
+}
+
+void PrintMatrix(int[,] matrix) // метод PrintMatrix печатающий нашу матрицу на экран
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write(matrix[i, j] + " "); // выводим матрицу на экран
+    }
+    Console.WriteLine(); // после обработки сроки по вышеуказанному циклу и до перехода к следующей выводим обработанную строку
+  }
+}
+
+int rowsCount = ReadNumber("Введите колличество строк:");
+int columnsCount = ReadNumber("Введите колличество столбцов:");
+int userRow = ReadNumber("Введите индекс строки нужного элемента");
+int userColumn = ReadNumber("Введите индекс стобца нужного элемента");
+
+int[,] myMatrix = GetRandomMatrix(rowsCount, columnsCount);
+
+PrintMatrix(myMatrix); // вызываем метод по распечатке и распечатываем получившуюся матрицу
+Console.WriteLine(); // делаем строчку чтоб выведенные матрицы не сливались
+
+MatrixSquare(myMatrix, userRow, userColumn);
---
+++ Task_52/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 52: 
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadNumber(string messageToUser) // метод преобразования строки заданной пользователем в число
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[, ] GetRandomMatrix(int rows, int columns, int leftBorder = 0, int rightBorder = 10) // метод по созданию матрицы
+
+{
+    int [, ] matrix = new int[rows, columns]; // выделяем память под массив
+
+  for(int i = 0; i < matrix.GetLength(0); i++) // цикл по созданию строк
+  
+  {
+      for(int j = 0; j < matrix.GetLength(1); j++) // цикл по созданию столбцов 
+      {
+        matrix [i, j] = Random.Shared.Next(leftBorder, rightBorder + 1); // создаем рандомную матрицу
+      }
+  }
+  return matrix;
+
+}
+
+void PrintMatrix (int[,] matrix) // метод PrintMatrix печатающий нашу матрицу на экран
+{
+  for(int i = 0; i < matrix.GetLength(0); i++) 
+  {
+      for(int j = 0; j < matrix.GetLength(1); j++)
+      {
+        Console.Write(matrix [i, j] + " "); // выводим матрицу на экран
+      }
+      Console.WriteLine(); // после обработки сроки по вышеуказанному циклу и до перехода к следующей выводим обработанную строку
+  }
+}
+
+int m = ReadNumber("Введите колличество строк:");
+int n = ReadNumber("Введите колличество столбцов:");
+int [,] myMatrix = GetRandomMatrix(m, n); // создаем матрицу методом описанным выше
+PrintMatrix(myMatrix); // распечатали матрицу при помощи метода
+
+double average = 0;
+
+for (n = 0; n < myMatrix.GetLength(1); n++)
+{
+    average = 0;
+    for (m = 0; m < myMatrix.GetLength(0); m++)
+    {
+        average += myMatrix[m, n];
+        
+    }
+    average = average / myMatrix.GetLength(0);
+    Console.WriteLine($"Среднее значение для всех чисел столбца {average:f2}");
+}
